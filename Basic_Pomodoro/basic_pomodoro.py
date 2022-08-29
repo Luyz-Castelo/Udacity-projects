64267f1bb5aaa5fcd8e22dafdbe89aca0b572a3d
lcbranco <luyzcastelobranco@gmail.com>
2022-08-29 13:40:39 -0300

    Addition of a print and change of url

--- Basic_Pomodoro/basic_pomodoro.py
+++ Basic_Pomodoro/basic_pomodoro.py
@@ -1,10 +1,11 @@
 import webbrowser
 import time
   
-url = "https://www.youtube.com/watch?v=npWuDwefXc0"
+url = "https://www.youtube.com/watch?v=kxGWsHYITAw"
 
+print("This program started in", time.ctime())
 num = 0
 while num < 3:
   time.sleep(7200) # 2 hours
-  webbrowser.open(url) # video pra te lembrar de ter um break
+  webbrowser.open(url) # video pra te lembrar de ter um break de 20 min
   num += 1
